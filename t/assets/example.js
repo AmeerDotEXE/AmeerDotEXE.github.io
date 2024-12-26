56f5ed6c6e3814c31882d0754eb1c8552fb0ca09
AmeerDotEXE <ameerkhalid193@gmail.com>
2024-12-26 21:36:15 +0300

    fixed terminal example not loading right

--- t/assets/example.js
+++ t/assets/example.js
@@ -1,6 +1,12 @@
-loadModules.push(async function(command, terminal) {
-	if (command !== "e") return false;
-	await term.echo("EA Sports", { typing: true, delay: 100 });
-	await term.echo("It's in the Game!", { typing: true, delay: 50 });
-	return true;
-});
+(() => {
+	loadModules.push(function(command, terminal) {
+		if (command !== "e") return false;
+		onCommand();
+		return true;
+	});
+
+	async function onCommand() {
+		await term.echo("EA Sports", { typing: true, delay: 100 });
+		await term.echo("It's in the Game!", { typing: true, delay: 50 });
+	}
+})();
